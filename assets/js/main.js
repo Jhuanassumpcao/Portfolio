ad0321ae85f73b2d53174d98aad984bbd0ec0262
jhuanassumpcao <jhuanassumpcao@gmail.com>
2023-03-17 15:52:14 -0300

    Update perfil dinamico

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@ function updateProfileInfo(profileData){
 	const photo = document.getElementById('profile.photo')
 	photo.src = profileData.photo
 	photo.alt = profileData.name
+
+	const name = document.getElementById('profile.name')
+	name.innerText = profileData.name
+	
+
+	const job = document.getElementById('profile.job')
+	job.innerText = profileData.job
+	job.href = `${profileData.github}`
+
+	const phone = document.getElementById('profile.phone')
+	phone.innerText = profileData.phone
+	phone.href = `tel:${profileData.phone}`
+
+	const email = document.getElementById('profile.email')
+	email.innerText = profileData.email
+	email.href = `mailto:${profileData.email}`
+
+	const location = document.getElementById('profile.location')
+	location.innerText = profileData.location
 }
 
 
