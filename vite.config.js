062247270585df7071d8da1c9522b44f435287d5
Jhuanassumpcao <jhuanassumpcao@gmail.com>
2024-11-06 18:36:21 -0300

    fix: path deploy

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,7 +9,11 @@ export default defineConfig({
 		port: 8080,
 	},
 
-  base: '/Portfolio/',
+  resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
   
   plugins: [vue()]
 })
